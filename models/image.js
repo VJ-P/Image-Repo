936aef498bd391a1f818949e8d98dc0f2808e520
Vijay Patel <108.vijayp@gmail.com>
2021-01-12 16:06:19 -0500

    Create intial MongoDB model for an Image

---
+++ models/image.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create the schema for an image item
+const imageSchema = new Schema ({
+    "user_id": String,
+    "url": String,
+    "private": Boolean
+});
+
+module.exports = mongoose.model('Image', imageSchema);
