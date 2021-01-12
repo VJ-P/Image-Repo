09a62bc44bd2058a54716bf3504d4cf5b9fcec16
Vijay Patel <108.vijayp@gmail.com>
2021-01-12 16:21:40 -0500

    Create connection to mongodb database, and check connection

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+const path = require('path');
 const express = require("express");
-const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
+// Add connection to local mongodb database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/image-repo', {
+    useNewUrlParser: true,
+    useCreateIndex: true,  // Uses createIndex() instead of ensureIndex() to avoid deprecation warnings
+    useUnifiedTopology: true
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+// Test mongodb connection
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("Database connected");
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render("home");
+});
+
+app.listen(3000, () => {
+    console.log("Example app listening at port 3000");
 })
