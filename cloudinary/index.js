cb52d794897d3ee449de0f5acdb0d5417f8208e5
Vijay Patel <108.vijayp@gmail.com>
2021-01-16 16:14:49 -0500

    Add support for uploading and displaying GIFs

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -13,7 +13,7 @@ const storage = new CloudinaryStorage({
     cloudinary,
     params: {
         folder:'Image-Repo',
-        allowedFormats: ['jpeg', 'png', 'jpg']
+        allowedFormats: ['jpeg', 'png', 'jpg', 'gif']
     }
 });
 
